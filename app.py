ed43358abc611ff0fad660252d67bee8ff0ab065
unknown <natnaelm552@gmail.com>
2024-09-21 16:14:21 +0300

    rag

---
+++ app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+from langchain.prompts import ChatPromptTemplate
+from langchain.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain_groq import ChatGroq
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from dotenv import load_dotenv
+import os
+import time
+
+load_dotenv()
+
+# Set the environment variable for the GROQ API key
+os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
+
+if "vector" not in st.session_state:
+    st.session_state.embeddings=HuggingFaceEmbeddings()
+    st.session_state.loader=WebBaseLoader("https://docs.smith.langchain.com/")
+    st.session_state.docs=st.session_state.loader.load()
+
+    st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+    st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
+    st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings)
+
+
+# Title for the Streamlit app
+st.title('LangChain Chat with GROQ API')
+
+# Create an instance of the ChatGroq language model
+llm = ChatGroq(temperature=0.5, model_name="mixtral-8x7b-32768")
+
+# Define the prompt template
+prompt=ChatPromptTemplate.from_template(
+"""
+Answer the questions based on the provided context only.
+Please provide the most accurate response based on the question
+<context>
+{context}
+<context>
+Questions:{input}
+
+"""
+)
+
+# Create a document chain for combining documents
+document_chain = create_stuff_documents_chain(llm, prompt)
+retriever = st.session_state.vectors.as_retriever()
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+prompt=st.text_input("Input you prompt here")
+
+if prompt:
+    start=time.process_time()
+    response=retrieval_chain.invoke({"input":prompt})
+    print("Response time :",time.process_time()-start)
+    st.write(response['answer'])
+
+    # With a streamlit expander
+    with st.expander("Document Similarity Search"):
+        # Find the relevant chunks
+        for i, doc in enumerate(response["context"]):
+            st.write(doc.page_content)
+            st.write("--------------------------------")
